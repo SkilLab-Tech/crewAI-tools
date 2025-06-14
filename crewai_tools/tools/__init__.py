92cb26079d03aa514d5f172ea4d943253d64f697
SkilLab HR and Tech <ivan@skillab.com.br>
2025-06-13 22:56:03 -0300

    chore: sync submodule changes

--- crewai_tools/tools/__init__.py
+++ crewai_tools/tools/__init__.py
@@ -67,6 +67,7 @@ from .snowflake_search_tool import (
     SnowflakeSearchToolInput,
 )
 from .spider_tool.spider_tool import SpiderTool
+from .stagehand_tool.stagehand_tool import StagehandTool
 from .txt_search_tool.txt_search_tool import TXTSearchTool
 from .vision_tool.vision_tool import VisionTool
 from .weaviate_tool.vector_search import WeaviateVectorSearchTool
@@ -76,3 +77,4 @@ from .youtube_channel_search_tool.youtube_channel_search_tool import (
     YoutubeChannelSearchTool,
 )
 from .youtube_video_search_tool.youtube_video_search_tool import YoutubeVideoSearchTool
+from .zapier_action_tool.zapier_action_tool import ZapierActionTools
